1dee35cb4aaefc6da45db6560f1854c53908c935
nductoan <toanbeo090400@gmail.com>
2022-10-01 13:34:44 +0700

    middleware jwt

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,10 @@
 var express = require("express");
 var router = express.Router();
 const userController = require("./../controllers/authController");
-const middlewareController = require("./../controllers/middlewareController");
 
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.post("/refresh", userController.refresh);
-router.post("/logout", middlewareController.verifyToken, userController.logout);
+router.post("/logout", userController.logout);
 
 module.exports = router;
